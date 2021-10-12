9261f41c99d57e55ae4d75dbd019353c27855a54
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-10-12 10:12:34 -0700

    Dev environment setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'client/index.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|png)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  }
+}
